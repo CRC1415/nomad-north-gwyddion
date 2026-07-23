3345eec8ff5fd9c12857467948ebfd4de6df20aa
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2026-07-23 19:36:08 +0200

    Update test_north_tools.py

--- tests/north_tools/test_north_tools.py
+++ tests/north_tools/test_north_tools.py
@@ -1,9 +1,9 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails
-    from nomad_north_gwyddion.north_tools import north_entry_point
+    from nomad_north_gwyddion.north_tools import gwyddion
 
     expected_id = 'nomad-north-gwyddion-my-north-tool'
     assert (
-        north_entry_point.id_url_safe == expected_id
-        or north_entry_point.id == 'nomad-north-nomad-north-gwyddion'
+        gwyddion.id_url_safe == 'gwyddion'
+        or gwyddion.id == 'nomad-north-gwyddion'
     ), 'NORTHTool entry point has incorrect id or id_url_safe'
